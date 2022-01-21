91ec8bb54323dbf37cd75c6620b6873eef8d466b
Esteban A. Giménez <estebanalfons.gimenez@murciaeduca.es>
2022-01-21 16:58:32 +0100

    Modificación comboBox y añadir .gitignore

--- src/main/java/es/ideas/PrimaryController.java
+++ src/main/java/es/ideas/PrimaryController.java
@@ -2,12 +2,23 @@ package es.ideas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 
-public class PrimaryController implements Initializable{
+public class PrimaryController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> cbSemana;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) { 
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        //Inicialización del ComboBox con los días de la semana
+        //También tiene que traducirse
+        String dias_semana[] = {"lunes", "martes", "miércoles", "jueves",
+            "viernes", "sábado", "domingo"};
+        cbSemana.setItems(FXCollections.observableArrayList(dias_semana));        
     }
-    
 }
