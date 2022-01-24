6da511d206e00631679ecd80f7cf042a646265d8
Abel <1727407@alu.murciaeduca.es>
2022-01-24 17:01:24 +0100

    Botones e imágenes añadidos a la interfaz. Tamaño de escena cambiado. Tamaño de escena fijo.

--- src/main/java/es/ideas/MultiLenguajeUI.java
+++ src/main/java/es/ideas/MultiLenguajeUI.java
@@ -17,8 +17,9 @@ public class MultiLenguajeUI extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 642, 666);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
