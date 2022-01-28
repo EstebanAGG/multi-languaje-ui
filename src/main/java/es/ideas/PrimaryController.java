092b9d97ed8882ab817c8850ee90df49c95ae23b
FranToMo <7787918@alu.murciaeduca.es>
2022-01-28 15:59:50 +0100

    Modifico el código de las clases principal y controladora, instancio un ToggleGroup al que añado los botones para cambiar el idioma, creo el directorio i18n donde añado un fichero 'properties' para cada idioma adicional y el principal de nombre 'idioma'

--- src/main/java/es/ideas/PrimaryController.java
+++ src/main/java/es/ideas/PrimaryController.java
@@ -1,16 +1,30 @@
 package es.ideas;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 public class PrimaryController implements Initializable {
 
     @FXML
     private ChoiceBox<String> cbSemana;
+    @FXML
+    private ToggleButton tbtnES, tbtnIT, tbtnIn, tbtnFr;
+    private MultiLenguajeUI main;
+
+    public void setMain(MultiLenguajeUI main) {
+        this.main = main;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -20,5 +34,22 @@ public class PrimaryController implements Initializable {
         String dias_semana[] = {"lunes", "martes", "miércoles", "jueves",
                 "viernes", "sábado", "domingo"};
         cbSemana.setItems(FXCollections.observableArrayList(dias_semana));
+
+
+        /*Inicialización del toggleGroup
+         * */
+        ToggleGroup toggleGroup = new ToggleGroup();
+
+        toggleGroup.getToggles().addAll(tbtnES, tbtnIT, tbtnIn,tbtnFr);
+
+
+    }
+
+    private FXMLLoader getLoader() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setResources(ResourceBundle.getBundle("es.ideas.i18n/idioma",
+                Locale.getDefault()));
+        loader.setLocation(getClass().getResource("primary.fxml"));
+        return loader;
     }
 }
