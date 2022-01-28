e5ef78a6a7a53ce5156dce454cb8bf79f6190bd7
FranToMo <7787918@alu.murciaeduca.es>
2022-01-28 16:58:21 +0100

    Añado comentarios e indento el código

--- src/main/java/es/ideas/MultiLenguajeUI.java
+++ src/main/java/es/ideas/MultiLenguajeUI.java
@@ -1,30 +1,30 @@
 package es.ideas;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
 /**
- * JavaFX App
+ * JavaFX App, método principal
  */
 public class MultiLenguajeUI extends Application {
-
     private static Scene scene;
+    /*Declaro un objeto de la clase Stage*/
     private static Stage stagePrimary;
-
     @Override
     public void start(Stage stage) throws IOException {
+        /*Instancio el objeto de la clase Stage previamente creado con valor de esta clase
+        * que recibe el método start como parámetro */
         this.stagePrimary = stage;
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("primary.fxml"));
-        loader.setResources(ResourceBundle.getBundle("es.ideas.i18n/idioma",
-                Locale.getDefault()));
+        /*
+        * Cargo el directo de recursos que contiene los archivos properties
+        * y el Locale por defecto del sistema*/
+        loader.setResources(ResourceBundle.getBundle("es.ideas.i18n/idioma", Locale.getDefault()));
         Parent parent = loader.load();
         PrimaryController primaryController = loader.getController();
         primaryController.setMain(this);
@@ -32,14 +32,12 @@ public class MultiLenguajeUI extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
-
+    /*Método getter de la instancia del objeto del tipo Stage
+    * que utilizare desde el controlador*/
     public static Stage getStagePrimary(){
         return stagePrimary;
     }
-
     public static void main(String[] args) {
         launch();
     }
-
 }
--- src/main/java/es/ideas/PrimaryController.java
+++ src/main/java/es/ideas/PrimaryController.java
@@ -1,8 +1,6 @@
 package es.ideas;
-
 import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -15,22 +13,22 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 
 public class PrimaryController implements Initializable {
-
     @FXML
     private ChoiceBox<String> cbSemana;
     @FXML
     private ToggleButton tbtnES, tbtnIT, tbtnIn, tbtnFr;
     private MultiLenguajeUI main;
 
-    public void setMain(MultiLenguajeUI main) {
-        this.main = main;
-    }
+    public void setMain(MultiLenguajeUI main) {this.main = main;}
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        //Inicialización del ComboBox con los días de la semana
-        //También tiene que traducirse
+        /*
+        * Inicialización del ComboBox con los días de la semana
+        * mediane el metodo getString de la clase abstracta ResourceBundle
+        * que devuelve un string e introduce la key que se le pasa como parámetro
+        * construyo el array de string que representa cada uno de losa elementos
+        * del ChoiceBox*/
         String dias_semana[] = {rb.getString("l"),
                 rb.getString("m"),
                 rb.getString("x"),
@@ -39,54 +37,43 @@ public class PrimaryController implements Initializable {
                 rb.getString("s"),
                 rb.getString("d")};
         cbSemana.setItems(FXCollections.observableArrayList(dias_semana));
-        cbSemana.setItems(FXCollections.observableArrayList(dias_semana));
-
-
-        /*Inicialización del toggleGroup
-         * */
+        /*
+        * Se inicializa el ToggleGroup y se le añaden los botones encargados de seleccionar el idioma
+        * y el listener */
         ToggleGroup toggleGroup = new ToggleGroup();
-
         toggleGroup.getToggles().addAll(tbtnES, tbtnIT, tbtnIn,tbtnFr);
         toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            /*
+            * Se modifica el valor de Locale en función del texto que contenga el botón pulsado*/
             if (newValue != null) {
-                ToggleButton toggleButton = (ToggleButton) newValue
-                        .getToggleGroup()
-                        .getSelectedToggle();
-
+                ToggleButton toggleButton = (ToggleButton) newValue.getToggleGroup().getSelectedToggle();
                 switch (toggleButton.getText()) {
-
-                    case "Italiano":
-                        Locale.setDefault(Locale.ITALIAN);
+                    case "Italiano":Locale.setDefault(Locale.ITALIAN);
                         break;
-                    case "Inglés":
-                        Locale.setDefault(Locale.US);
+                    case "Inglés":Locale.setDefault(Locale.US);
                         break;
-                    case "Frances":
-                        Locale.setDefault(Locale.FRENCH);
+                    case "Frances":Locale.setDefault(Locale.FRENCH);
                         break;
-                    default:
-                        Locale.setDefault(new Locale("es"));
+                    default:Locale.setDefault(new Locale("es"));
                 }
-
+                /* Se carga de nuevo el parent mediante el método getLoader
+                   que modifica el valor de Locale con el nuevo valor que ha recibido
+                *  tras su modificación en el switch anterior*/
                 try {
                     Parent parent = getLoader().load();
                     MultiLenguajeUI.getStagePrimary().getScene().setRoot(parent);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                MultiLenguajeUI.getStagePrimary().show();
-
-
-            }
-        });
-
-
-    }
-
+                MultiLenguajeUI.getStagePrimary().show();}});
+}
+/*Método que devuelve un objeto de la clase FXMLLoader
+modificado con los nuevos valores por defecto de la clase Locale
+recibidos al pulsar un botón de seleccion de idioma
+* */
     private FXMLLoader getLoader() {
         FXMLLoader loader = new FXMLLoader();
-        loader.setResources(ResourceBundle.getBundle("es.ideas.i18n/idioma",
-                Locale.getDefault()));
+        loader.setResources(ResourceBundle.getBundle("es.ideas.i18n/idioma",Locale.getDefault()));
         loader.setLocation(getClass().getResource("primary.fxml"));
         return loader;
     }
